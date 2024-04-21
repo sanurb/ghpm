146fe266f1cdcf23820a82fb9eb23162becc8251
Santiago U <david.urbano@uao.edu.co>
2024-04-21 18:18:45 -0500

    feat: add Filter by recent activity?

--- internal/command/command.go
+++ internal/command/command.go
@@ -1,6 +1,15 @@
 package command
 
-import "github.com/sanurb/ghpm/internal/repo"
+import (
+	"bufio"
+	"fmt"
+	"github.com/sanurb/ghpm/internal/github"
+	"github.com/sanurb/ghpm/internal/repo"
+	"github.com/sanurb/ghpm/internal/ui"
+	"os"
+	"os/exec"
+	"strings"
+)
 
 type Command interface {
 	Execute() error
@@ -32,4 +41,53 @@ type CloneOthersCommand struct {
 
 func (c *CloneOthersCommand) Execute() error {
 	return repo.CloneOthersRepos(c.Username)
+}
+
+type FilteredCloneCommand struct {
+	Days int
+}
+
+func (c *FilteredCloneCommand) Execute() error {
+
+	repos, err := github.FetchUserRepos(nil)
+	if err != nil {
+		return fmt.Errorf("failed to fetch user repos: %w", err)
+	}
+
+	filteredRepos, err := repo.FilterRecentRepos(repos, c.Days)
+	if err != nil {
+		return fmt.Errorf("failed to filter recent repos: %w", err)
+	}
+
+	if len(filteredRepos) == 0 {
+		fmt.Println("No recent repositories to clone.")
+		return nil
+	}
+
+	fmt.Printf("Found %d recent repositories to clone.\n", len(filteredRepos))
+	ui.DisplayReposTable(filteredRepos)
+	if confirmAction("Proceed with cloning?") {
+		for _, repo := range filteredRepos {
+			fmt.Printf("Cloning %s...\n", repo.HTMLURL)
+			cmd := exec.Command("git", "clone", repo.HTMLURL)
+			if err := cmd.Run(); err != nil {
+				fmt.Fprintf(os.Stderr, "Failed to clone %s: %v\n", repo, err)
+				continue
+			}
+		}
+	}
+
+	return nil
+}
+
+func confirmAction(message string) bool {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("%s [y/n]: ", message)
+	response, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading input: %v\n", err)
+		os.Exit(1)
+	}
+	response = strings.TrimSpace(response)
+	return strings.ToLower(response) == "y"
 }
